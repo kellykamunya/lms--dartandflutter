cbc9210bde8937a4f2c3be747bec69f80d0eefff
kellykamunya <132022927+kellykamunya@users.noreply.github.com>
2024-02-29 13:19:13 -0800

    Add files via upload

---
+++ datatypes/main.dart
@@ -0,0 +1,13 @@
+//datatypes in dart
+void main(){
+  int money=5000;//this is the interger datatype shows whole numbers without floating point
+  print("the amount of money is $money kenyan shillings");
+  double litres=3.5;//double which is numbers with floating points
+  print("the cow produced $litres litres today");
+  String herNames="caroline wambui";//example of strings datatype which include words or sentences
+  print("my cousin's name is $herNames");
+  List<String> cars=["Mercedes Benz", "Subaru", "BMW", "Range rover"];//this is list datatype that is used to list various items in a program
+  print("my current favorite car models are: $cars");
+  Map<String, int>carPrices={"mercedes Benz":5000000, "BMW":7000000};//Map datatype hope keys and values
+  print(carPrices);
+}
